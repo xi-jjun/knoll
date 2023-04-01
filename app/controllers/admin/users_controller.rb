a894b9f3837b98506ef96aafd144a5b18e259261
xi_jjun <rlawowns97@naver.com>
2023-04-01 17:34:20 +0900

    #5 : 동산 회원관리 어드민 페이지 layout 추가 및 적용

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,8 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_staff!
 
+  layout 'admin_application'
+
   def index
     @users = User.all
   end
--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -1,4 +1,8 @@
 class Admin::WelcomeController < ApplicationController
+  before_action :authenticate_staff!
+
+  layout 'admin_application'
+
   def index
 
   end
